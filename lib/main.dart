824d131f3df862a5f0ba1db3bc5801f76820b4fd
JunJangE <wnswkd486@gmail.com>
2021-09-08 23:28:41 +0900

    21.09.08

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: '김우탄',
+      home: MyCard(),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[800],
+      appBar: AppBar(
+        title: Text("노원구 녹차밭"),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+        elevation: 0.0,
       ),
-      home: RootPage(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/wootan.png'),
+                radius: 80.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[850],
+              thickness: 0.5,
+              endIndent: 30.0,
+            ),
+            Text("Name",
+                style: TextStyle(
+                    color: Colors.white, letterSpacing: 2.0, fontSize: 23.0)),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "김우탄",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text("Power Level",
+                style: TextStyle(
+                    color: Colors.white, letterSpacing: 2.0, fontSize: 23.0)),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "14LV",
+              style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "Using lightsaber",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "face hero tattoo",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  "fire flames",
+                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
+                ),
+              ],
+            ),
+            // Center(
+            //   child: CircleAvatar(
+            //     backgroundImage: AssetImage('assets/ma.png'),
+            //     radius: 40.0,
+
+            //   ),
+            // )
+          ],
+        ),
+      ),
     );
   }
 }
