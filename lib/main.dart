168fffcccac97f3d25d0a502737f9623c22ccc4e
JunJangE <wnswkd486@gmail.com>
2021-09-12 21:57:30 +0900

    21.09.12

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,82 @@ class MyCard extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green[700],
         elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              print("shopping button is clicked");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("serach button is clicked");
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/wootan.png'),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/ma.png'),
+                  backgroundColor: Colors.white,
+                ),
+              ],
+              accountName: Text('김우탄'),
+              accountEmail: Text('mdsoo55828@gmail.com'),
+              onDetailsPressed: () {
+                print("arrow is clicked");
+              },
+              decoration: BoxDecoration(
+                  color: Colors.green[700],
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10.0),
+                      bottomRight: Radius.circular(10.0))),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: () {
+                print('Home is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('Setting'),
+              onTap: () {
+                print('Setting is clicked');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked');
+              },
+              trailing: Icon(Icons.add),
+            )
+          ],
+        ),
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
@@ -47,34 +123,34 @@ class MyCard extends StatelessWidget {
             ),
             Text("Name",
                 style: TextStyle(
-                    color: Colors.white, letterSpacing: 2.0, fontSize: 23.0)),
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold)),
             SizedBox(
               height: 10.0,
             ),
             Text(
               "김우탄",
               style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold),
+                  color: Colors.white, letterSpacing: 2.0, fontSize: 18.0),
             ),
             SizedBox(
               height: 30.0,
             ),
             Text("Power Level",
                 style: TextStyle(
-                    color: Colors.white, letterSpacing: 2.0, fontSize: 23.0)),
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 23.0,
+                    fontWeight: FontWeight.bold)),
             SizedBox(
               height: 10.0,
             ),
             Text(
-              "14LV",
+              "23LV",
               style: TextStyle(
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold),
+                  color: Colors.white, letterSpacing: 2.0, fontSize: 18.0),
             ),
             SizedBox(
               height: 30.0,
@@ -122,8 +198,38 @@ class MyCard extends StatelessWidget {
 
             //   ),
             // )
+            MySnackBar(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MySnackBar extends StatefulWidget {
+  const MySnackBar({Key? key}) : super(key: key);
+
+  @override
+  _MySnackBarState createState() => _MySnackBarState();
+}
+
+class _MySnackBarState extends State<MySnackBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Show me'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("안녕! 나는 우탄~",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white)),
+              backgroundColor: Colors.teal,
+              duration: Duration(milliseconds: 1000),
+            ),
+          );
+        },
       ),
     );
   }
