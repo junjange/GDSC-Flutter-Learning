7e6a2c170d069c658df5f5bd3ffbc919b0174f0f
JunJangE <wnswkd486@gmail.com>
2021-10-27 18:41:11 +0900

    21.10.27

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,87 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:instagrem_clon/dice.dart';
+import 'dice.dart';
 
-<<<<<<< Updated upstream
-
-int cnt = 0;
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-=======
 class LoginPage extends StatefulWidget {
   @override
   _LoginPage createState() => _LoginPage();
 }
->>>>>>> Stashed changes
 
 class _LoginPage extends State<LoginPage> {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
 
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-<<<<<<< Updated upstream
-      backgroundColor: Colors.blue[200],
-      appBar: AppBar(title: Text("hi")),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
-
-        children: <Widget>[
-          Text("버튼을 누른 횟수"),
-          Text("$cnt", style: Theme.of(context).textTheme.display1,),
-          Row(mainAxisAlignment: MainAxisAlignment.center,
-
-          children: <Widget> [FloatingActionButton(child: Icon(Icons.add),onPressed: (){
-            setState(() {
-              
-              cnt++;
-              print("$cnt");
-            });
-
-          }),
-          Padding(padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),),
-          FloatingActionButton(child: Icon(Icons.remove),onPressed: (){
-            setState(() {
-            
-              
-              print("$cnt");
-              if (cnt >= 1){
-                  cnt--;
-              }
-            });
-
-          }),
-          ]),
-          ],
-         
-      
-      )
-    ),
-    // floatingActionButton: FloatingActionButton(
-    //   child: Icon(Icons.add),
-    //   onPressed: (){
-    //     setState(() { // 
-    //     cnt++;
-    //     print("$cnt");
-    //     });
-        
-    //   },
-    // ),
-    
-
-    
-  );
-}
-=======
-        appBar: AppBar(
-          title: Text("Log in"),
-          backgroundColor: Colors.redAccent,
-          centerTitle: true,
-        ),
-        body: SingleChildScrollView(
+      appBar: AppBar(
+        title: Text("Log in"),
+        backgroundColor: Colors.redAccent,
+        centerTitle: true,
+      ),
+      body: Builder(builder: (context) {
+        return SingleChildScrollView(
           child: Column(
             children: <Widget>[
               Padding(padding: EdgeInsets.only(top: 20)),
@@ -104,11 +43,13 @@ class _LoginPageState extends State<LoginPage> {
                         child: Column(
                           children: <Widget>[
                             TextField(
+                              controller: controller,
                               decoration:
                                   InputDecoration(labelText: 'Enter"dice"'),
                               keyboardType: TextInputType.emailAddress,
                             ),
                             TextField(
+                              controller: controller2,
                               decoration:
                                   InputDecoration(labelText: 'Enter Passwrod'),
                               keyboardType: TextInputType.text,
@@ -124,13 +65,56 @@ class _LoginPageState extends State<LoginPage> {
                                     color: Colors.orangeAccent,
                                     child: Icon(Icons.arrow_forward,
                                         color: Colors.white, size: 35.0),
-                                    onPressed: () {}))
+                                    onPressed: () {
+                                      if (controller.text == "dice" &&
+                                          controller2.text == "1234") {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder:
+                                                    (BuildContext context) =>
+                                                        Dice()));
+                                      } else if (controller.text == "dice" &&
+                                          controller2.text != "1234") {
+                                        showSnackerBar2(context);
+                                      } else if (controller.text != "dice" &&
+                                          controller2.text == "1234") {
+                                        showSnackerBar3(context);
+                                      } else {
+                                        showSnackerBar(context);
+                                      }
+                                    }))
                           ],
                         ),
                       )))
             ],
           ),
-        ));
+        );
+      }),
+    );
   }
->>>>>>> Stashed changes
+}
+
+void showSnackerBar(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text("로그인 정보를 다시 확인하세요.", textAlign: TextAlign.center),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackerBar2(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text("비밀번호가 일치하지 않습니다.", textAlign: TextAlign.center),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
+}
+
+void showSnackerBar3(BuildContext context) {
+  Scaffold.of(context).showSnackBar(SnackBar(
+    content: Text("dice의 철자를 확인하세요.", textAlign: TextAlign.center),
+    duration: Duration(seconds: 2),
+    backgroundColor: Colors.blue,
+  ));
 }
